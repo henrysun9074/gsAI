57dc70b1116699aaff0034b15800045e487383a8
Henry Sun <hs325@duke.edu>
2025-09-04 15:39:23 -0400

    running stuff

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,23 +2,29 @@
 #SBATCH --job-name=genomic_selection
 #SBATCH --output=genomic_selection.out
 #SBATCH --error=genomic_selection.err
-#SBATCH --partition=gpu-common
+#SBATCH --partition=gpu-common 
 #SBATCH --gres=gpu:1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=128G
 #SBATCH --exclusive
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hs325@duke.edu
 
 cd /work/tfs3/gsAI
 export LD_LIBRARY_PATH=/hpc/group/schultzlab/hs325/miniconda3/envs/gsAI/lib:$LD_LIBRARY_PATH
+module load CUDA
 
 echo "Job ID: $SLURM_JOB_ID"
 echo "Running on node: $(hostname)"
 echo "Number of CPUs: $SLURM_CPUS_PER_TASK"
 
-module load CUDA
-conda activate gsAI
+# PYTHON_EXEC=/hpc/group/schultzlab/hs325/miniconda3/envs/gsAI/bin/python
+# # sanity check
+# echo "Using Python: $($PYTHON_EXEC --version)"
+# $PYTHON_EXEC -m pip list | grep -E "numpy|scikit-learn|xgboost"
+# $PYTHON_EXEC genomic_selection_pipeline.py
 
+source /hpc/group/schultzlab/hs325/miniconda3/etc/profile.d/conda.sh
+conda activate gsAI
 python genomic_selection_pipeline.py
