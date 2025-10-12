b46b82ebfe47f03394d80ab5bf623eb8f95a4594
Henry Sun <hs325@duke.edu>
2025-10-12 15:32:02 +0000

    preliminary stats testing

---
+++ 04_stats.R
@@ -0,0 +1,297 @@
+###############################################################################
+
+## Set WD and load packages 
+
+setwd("/work/tfs3/gsAI")
+library(tidyverse)
+library(readxl)
+library(vegan)
+library(RColorBrewer)
+library(broom)
+library(car)
+library(rstatix)
+library(FSA)
+library(emmeans)
+
+###############################################################################
+
+## load data for AI vs R comparison
+
+ml_models_path <- "/work/tfs3/gsAI/gebvs/Oct08_allnoqc_GEBVs_10foldCV.csv"
+r_models_path <- "/work/tfs3/gsAI/4paul/gebvs/Oct01_CrossValDarpaGebv_noQC.xlsx"
+ml_metrics_path <- "/work/tfs3/gsAI/gebvs/Oct08_allnoqc_fold_metrics.csv"
+r_metrics_path <- "/work/tfs3/gsAI/4paul/gebvs/Oct01_CrossValDarpaFoldMetrics.csv"
+
+DARPAGEBV <- read.csv(ml_models_path) 
+DARPAGEBV2 <- read_xlsx(r_models_path) 
+df <- merge(DARPAGEBV, DARPAGEBV2)
+
+MLMETRICS <- read.csv(ml_metrics_path) 
+RMETRICS <- read.csv(r_metrics_path)
+names(RMETRICS) <- tolower(names(RMETRICS))
+names(MLMETRICS) <- tolower(names(MLMETRICS))
+MLMETRICS <- MLMETRICS %>%
+  rename(correlation = pearsonr)
+RMETRICS <- RMETRICS %>%
+  mutate(model=recode(model, "BL" = "LASSO"))
+
+MLMETRICS <- MLMETRICS %>%
+  group_by(model) %>%
+  mutate(iteration = rep(1:10, each = 5, length.out = n())) %>%
+  ungroup()
+
+allMetrics <- rbind(MLMETRICS, RMETRICS)
+
+per_iter <- allMetrics %>%
+  group_by(model, iteration) %>%
+  summarise(
+    auc_iter = mean(auc, na.rm = TRUE),
+    corr_iter = mean(correlation, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+#### ANOVA assumptions check
+
+# Normality
+models <- unique(per_iter$model)
+models <- models[models != "GBLUP"]
+for (m in models) {
+  cat("Model:", m)
+  data_m <- per_iter %>% filter(model == m)
+  qqPlot(data_m$corr_iter, main = paste("QQ Plot -", m))
+  shapiro_res <- shapiro.test(data_m$corr_iter)
+  print(shapiro_res)
+}
+# all pass
+
+# Variance
+levene_res <- car::leveneTest(corr_iter ~ model, data = per_iter)
+print(levene_res)
+# for no QC: p < 0.001 ***, not ok!
+
+
+############# Hypothesis Testing
+
+models <- unique(per_iter$model)
+
+## Visualize results
+ggplot(per_iter, aes(x = model, y = corr_iter, fill = model)) +
+  geom_boxplot(alpha = 1) +
+  labs(x = "Model", y = "Correlation") +
+  theme_classic(base_size = 14) +
+  theme(legend.position = "none")
+
+#### ANOVA
+anova_res <- aov(corr_iter ~ model, data = per_iter)
+anova_posthoc <- TukeyHSD(anova_res)
+print(anova_posthoc)
+emmeans(anova_res, pairwise ~ model)
+
+tukey_df <- as.data.frame(anova_posthoc$model)
+tukey_df$comparison <- rownames(tukey_df)
+rownames(tukey_df) <- NULL
+nonsig_tukey <- tukey_df %>%
+  dplyr::filter(`p adj` > 0.05)
+
+print(nonsig_tukey)
+
+#### KW tests
+kruskal_res <- kruskal.test(corr_iter ~ model, data = per_iter)
+print(kruskal_res)
+dunn_res <- dunnTest(corr_iter ~ model, data = per_iter, method = "holm")
+print(dunn_res)
+
+dunn_table <- dunn_res$res
+nonsig <- dunn_table %>%
+  dplyr::filter(P.adj > 0.05)
+print(nonsig)
+
+###############################################################################
+
+## load data for F2 vs all comparison for AI models
+
+f2_ai <- "/work/tfs3/gsAI/gebvs/Oct07_F2noqc_fold_metrics.csv"
+all_ai <- "/work/tfs3/gsAI/gebvs/Oct08_allnoqc_fold_metrics.csv"
+
+f2_ai <- read.csv(f2_ai) 
+all_ai <- read.csv(all_ai)
+names(f2_ai) <- tolower(names(f2_ai))
+names(all_ai) <- tolower(names(all_ai))
+
+f2_ai <- f2_ai %>%
+  mutate(model = dplyr::recode(model,
+                               "LR" = "LR_F2",
+                               "RF" = "RF_F2",
+                               "GB" = "GB_F2"))
+
+all_ai <- all_ai %>%
+  mutate(model = dplyr::recode(model,
+                               "LR" = "LR_all",
+                               "RF" = "RF_all",
+                               "GB" = "GB_all"))
+
+allMetrics <- rbind(f2_ai, all_ai)
+allMetrics <- allMetrics %>%
+  rename(correlation = pearsonr)
+
+allMetrics <- allMetrics %>%
+  group_by(model) %>%
+  mutate(iteration = rep(1:10, each = 5, length.out = n())) %>%
+  ungroup()
+
+per_iter <- allMetrics %>%
+  group_by(model, iteration) %>%
+  summarise(
+    auc_iter = mean(auc, na.rm = TRUE),
+    corr_iter = mean(correlation, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+######### Assumption checks
+models <- unique(per_iter$model)
+for (m in models) {
+  cat("Model:", m)
+  data_m <- per_iter %>% filter(model == m)
+  qqPlot(data_m$corr_iter, main = paste("QQ Plot -", m))
+  shapiro_res <- shapiro.test(data_m$corr_iter)
+  print(shapiro_res)
+}
+# all pass
+
+# Variance
+levene_res <- car::leveneTest(corr_iter ~ model, data = per_iter)
+print(levene_res)
+#fail
+
+## Visualize results
+ggplot(per_iter, aes(x = model, y = corr_iter, fill = model)) +
+  geom_boxplot(alpha = 1) +
+  labs(x = "Model", y = "Correlation") +
+  theme_classic(base_size = 14) +
+  theme(legend.position = "none")
+
+#### ANOVA
+anova_res <- aov(corr_iter ~ model, data = per_iter)
+anova_posthoc <- TukeyHSD(anova_res)
+print(anova_posthoc)
+emmeans(anova_res, pairwise ~ model)
+
+tukey_df <- as.data.frame(anova_posthoc$model)
+tukey_df$comparison <- rownames(tukey_df)
+rownames(tukey_df) <- NULL
+nonsig_tukey <- tukey_df %>%
+  dplyr::filter(`p adj` > 0.05)
+
+print(nonsig_tukey)
+
+#### KW tests
+kruskal_res <- kruskal.test(corr_iter ~ model, data = per_iter)
+print(kruskal_res)
+dunn_res <- dunnTest(corr_iter ~ model, data = per_iter, method = "holm")
+print(dunn_res)
+
+dunn_table <- dunn_res$res
+nonsig <- dunn_table %>%
+  dplyr::filter(P.adj > 0.05)
+print(nonsig)
+
+
+###############################################################################
+
+## load data for F2 vs all comparison for R models
+
+all_r <- "/work/tfs3/gsAI/4paul/gebvs/Oct01_CrossValDarpaFoldMetrics.csv"
+f2_r <- "/work/tfs3/gsAI/4paul/gebvs/Oct04_CrossValFoldMetrics_F2noQC.csv"
+
+f2_r <- read.csv(f2_r) 
+all_r <- read.csv(all_r)
+
+names(f2_r) <- tolower(names(f2_r))
+names(all_r) <- tolower(names(all_r))
+f2_r <- f2_r %>%
+  mutate(model=dplyr::recode(model, "BL" = "LASSO"))
+all_r <- all_r %>%
+  mutate(model=dplyr::recode(model, "BL" = "LASSO"))
+
+f2_r <- f2_r %>%
+  mutate(model = dplyr::recode(model,
+                               "GBLUP" = "GBLUP_F2",
+                               "LASSO" = "LASSO_F2",
+                               "EGBLUP" = "EGBLUP_F2",
+                               "RKHS" = "RKHS_F2",
+                               "BB" = "BB_F2",
+                               "BRR" = "BRR_F2"))
+
+all_r <- all_r %>%
+  mutate(model = dplyr::recode(model,
+                               "GBLUP" = "GBLUP_all",
+                               "LASSO" = "LASSO_all",
+                               "EGBLUP" = "EGBLUP_all",
+                               "RKHS" = "RKHS_all",
+                               "BB" = "BB_all",
+                               "BRR" = "BRR_all"))
+
+allMetrics <- rbind(f2_r, all_r)
+
+allMetrics <- allMetrics %>%
+  group_by(model) %>%
+  mutate(iteration = rep(1:10, each = 5, length.out = n())) %>%
+  ungroup()
+
+per_iter <- allMetrics %>%
+  group_by(model, iteration) %>%
+  summarise(
+    auc_iter = mean(auc, na.rm = TRUE),
+    corr_iter = mean(correlation, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+######### Assumption checks
+models <- unique(per_iter$model)
+for (m in models) {
+  cat("Model:", m)
+  data_m <- per_iter %>% filter(model == m)
+  qqPlot(data_m$corr_iter, main = paste("QQ Plot -", m))
+  shapiro_res <- shapiro.test(data_m$corr_iter)
+  print(shapiro_res)
+}
+# all pass except GBLUP
+
+# Variance
+levene_res <- car::leveneTest(corr_iter ~ model, data = per_iter)
+print(levene_res)
+#fail
+
+## Visualize results
+ggplot(per_iter, aes(x = model, y = corr_iter, fill = model)) +
+  geom_boxplot(alpha = 1) +
+  labs(x = "Model", y = "Correlation") +
+  theme_classic(base_size = 14) +
+  theme(axis.text.x = element_blank()) +
+  theme(legend.position = "top")
+
+#### ANOVA
+anova_res <- aov(corr_iter ~ model, data = per_iter)
+anova_posthoc <- TukeyHSD(anova_res)
+print(anova_posthoc)
+emmeans(anova_res, pairwise ~ model)
+
+tukey_df <- as.data.frame(anova_posthoc$model)
+tukey_df$comparison <- rownames(tukey_df)
+rownames(tukey_df) <- NULL
+nonsig_tukey <- tukey_df %>%
+  dplyr::filter(`p adj` > 0.05)
+
+print(nonsig_tukey)
+
+#### KW tests
+kruskal_res <- kruskal.test(corr_iter ~ model, data = per_iter)
+print(kruskal_res)
+dunn_res <- dunnTest(corr_iter ~ model, data = per_iter, method = "holm")
+print(dunn_res)
+
+dunn_table <- dunn_res$res
+nonsig <- dunn_table %>%
+  dplyr::filter(P.adj > 0.05)
+print(nonsig)
+
